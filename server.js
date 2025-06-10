9ab5c76e96c63326f263293641a5a927ea98f684
ukaba05 <ukaba05@gmail.com>
2025-06-10 11:56:42 +0800

    update game address

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var server = express();
 
 var bodyParser = require("body-parser");
-server.use(express.static(__dirname + "/public"));
+server.use(express.static(__dirname + "/public/Dodge"));
 server.use(bodyParser.urlencoded({extended:true}));
 server.use(bodyParser.json());
 
